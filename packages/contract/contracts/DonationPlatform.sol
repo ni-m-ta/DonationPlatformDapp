968428689decbb30054ed8dcbe473878ffce5ddc
ni-m-ta <tatsuyanisato@gmail.com>
2023-07-19 13:23:50 +0200

    test code for the smart contract

---
+++ packages/contract/contracts/DonationPlatform.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract DonationPlatformContract{
+    constructor() {
+        console.log("Constructor has been read");
+    }
+
+    function test() public {
+        console.log("test");
+    }
+}
