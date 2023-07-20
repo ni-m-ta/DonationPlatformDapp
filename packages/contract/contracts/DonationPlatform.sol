1e403e6a15a3165a8a9bfd4e4abdbfc0811e87be
ni-m-ta <tatsuyanisato@gmail.com>
2023-07-20 15:36:17 +0200

    deploy the smart contract

--- packages/contract/contracts/DonationPlatform.sol
+++ packages/contract/contracts/DonationPlatform.sol
@@ -5,11 +5,28 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract DonationPlatformContract{
-    constructor() {
-        console.log("Constructor has been read");
+    address public owner;
+
+    event ReceivedETH(address indexed sender, uint256 amount);
+    event WithdrawnETH(address indexed rreceiver, uint256 amount);
+
+    constructor() payable {
+        console.log("Constructor has been executed");
     }
 
-    function test() public {
-        console.log("test");
+    receive() external payable {
+        emit ReceivedETH(msg.sender, msg.value);
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw(uint256 amount) public payable {
+        require(amount <= address(this).balance, "Insufficient contract balance");
+
+        address payable receiver = payable(msg.sender);
+        receiver.transfer(amount);
+        emit WithdrawnETH(msg.sender, amount);
     }
 }
